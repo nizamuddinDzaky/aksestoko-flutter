4adec422978feea0a4a18454e90510cf740cf17b
Abdurrouf <roufroufrouf@gmail.com>
2021-01-20 13:04:31 +0700

    penambahan debugMode

---
+++ lib/main_qa.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:aksestokomobile/app/my_app.dart';
+import 'package:aksestokomobile/network/api_client.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:get/get.dart';
+
+const isProd = false;
+
+// bool get isDebugQA => valDebug == 1 || (kDebugMode || !isProd);
+bool get isDebugQA => valDebug == 1 || isDebugMode;
+
+bool get isDebugOnly => kDebugMode || isDebugMode;
+
+bool get isDebugMode => valDebug == 2;
+
+bool get isProdMode => valDebug == 0 || isProd;
+
+int valDebug = kDebugMode ? 2 : ((kDebugMode || !isProd) ? 1 : 0);
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
+void main() async {
+  if (!isProd) HttpOverrides.global = new MyHttpOverrides();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  ApiClient.addInterceptor();
+  Get.config(enableLog: false);
+  await Firebase.initializeApp();
+  return runApp(Phoenix(child: MyApp()));
+}
